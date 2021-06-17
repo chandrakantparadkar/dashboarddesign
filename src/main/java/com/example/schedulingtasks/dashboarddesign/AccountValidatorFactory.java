477e83aa9f82e4eb9ead224b67489cd5f207012b
chandrakantparadkar <chandrakant.paradkar@gmail.com>
2021-06-17 19:28:39 +0530

    changes as per Abhijeet's suggestions

---
+++ src/main/java/com/example/schedulingtasks/dashboarddesign/AccountValidatorFactory.java
@@ -0,0 +1,7 @@
+package com.example.schedulingtasks.dashboarddesign;
+
+public class AccountValidatorFactory {
+
+	public static void validate(Provider provider, AssessmentUser assessmentUser, AssessmentJobInput input,Long userId) {
+	}
+}
---
+++ src/main/java/com/example/schedulingtasks/dashboarddesign/AssessmentUtils.java
@@ -0,0 +1,10 @@
+package com.example.schedulingtasks.dashboarddesign;
+
+public class AssessmentUtils {
+
+	public static JobSubmissionStatus getJobSubmissionStatus(boolean b, int value, Object object, String message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
